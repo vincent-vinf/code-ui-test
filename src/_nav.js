7d62e359fb6348355fede578c294d84897c1a5cb
Vincent <857618786vin@gmail.com>
2023-02-20 19:19:39 +0800

    init

---
+++ src/_nav.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    component: 'CNavItem',
+    name: '主页',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+  },
+  {
+    component: 'CNavTitle',
+    name: '验证项',
+  },
+  {
+    component: 'CNavItem',
+    name: '查看',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: '添加',
+    to: '/theme/typography',
+    icon: 'cil-pencil',
+  },
+  {
+    component: 'CNavTitle',
+    name: '验证任务',
+  },
+  {
+    component: 'CNavItem',
+    name: '查看',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: '添加',
+    to: '/theme/typography',
+    icon: 'cil-pencil',
+  },
+]
